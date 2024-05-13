7151288221c7776a6212a46818b6fc0832d5546d
Jibe7 <jb7_chevrier@hotmail.com>
2024-05-14 00:35:00 +0200

    Added auto sizing by getting the screen size. Added checkboxes to choose if the password includes lowercases/uppercases/digits/special characters (did not implement the 'logic' yet). Removed the disability to modify the password. Next step could be to make the checkboxes functionnal. Updated the .exe

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 Far from finished but it is functionnal : you can generate a password of desired length from a .exe file.
 I want to learn making desktop application as well as web dev but I will prioritize the latest so this project may stay paused like that for some time. 
 # Project Motivation
-- I know there exist reliable and secure software that generates and store securately passwords, I would recommand to check : https://github.com/Lissy93/awesome-privacy and I recommand Dashlane or Bitwarden for your personnal use.
-- However, I like creating applications with UI and I want to improve at it. So I need no more reasons not to try to make things myself 🤠 ! 
+- I know there exist reliable and secure software that generates and store securately passwords, I would recommend to check : https://github.com/Lissy93/awesome-privacy and Dashlane or Bitwarden for your personnal use.
+- However, I like creating applications with UI and I want to improve at it. So I need no more reasons not to try to make (some) things myself 🤠 ! 
 ## Project Goals
 1. Create a basic .exe app to generate random passwords of desired length ✅
 2. Add settings property such as choosing if the password has numbers, uppercase, lowercase, special characters.
--- password_generator/passGen.py
+++ password_generator/passGen.py
@@ -1,24 +1,73 @@
 import customtkinter
 
+customtkinter.set_appearance_mode('System')
+customtkinter.set_default_color_theme('dark-blue') # blue, dark-blue, green, 
+
+from screeninfo import get_monitors
 import string
 import secrets
+
+DEFAULT_WIDTH = 400
+DEFAULT_HEIGHT = 400
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
         self.title("Password Generator")
-        self.geometry("400x400")
+
+        width = DEFAULT_WIDTH
+        height = DEFAULT_HEIGHT
+        for m in get_monitors():
+            if m.is_primary:
+                width = m.width
+                height = m.height
+        geometry_size = str(width // 2) + 'x' + str(height // 2)
+        self.geometry(geometry_size)
+
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure((0, 10), weight=0)
 
-        self.button = customtkinter.CTkButton(self, text="Generate Password", command=lambda: self.basicPassGen(self.entry.get()))
-        self.button.grid(row=1, column=0, padx=0, pady=0)
+        self.lowercasesCheckBox = customtkinter.CTkCheckBox(self,
+            text = "Use lowercases"
+        )
+        self.lowercasesCheckBox.grid(row=1, column=0, padx=(0, 40), pady=(0, 20))
+        
+        self.uppercasesCheckBox = customtkinter.CTkCheckBox(self,
+            text = "Use uppercases"
+        )
+        self.uppercasesCheckBox.grid(row=2, column=0, padx=(0, 40), pady=(0, 20))
+        
+        self.digitsCheckBox = customtkinter.CTkCheckBox(self,
+            text = "Use numbers"
+        )
+        self.digitsCheckBox.grid(row=3, column=0, padx=(0, 55), pady=(0, 20))
+        
+        self.punctuationCheckBox = customtkinter.CTkCheckBox(self,
+            text = "Use special characters"
+        )
+        self.punctuationCheckBox.grid(row=4, column=0, padx=(0, 0), pady=(0, 20))
+
+
+        self.button = customtkinter.CTkButton(self, 
+            text="Generate Password", 
+            command=lambda: self.basicPassGen(self.entry.get())
+        )
+        self.button.grid(row=5, column=0, padx=0, pady=0)
 
         self.entry = customtkinter.CTkEntry(self, placeholder_text="Length of the password")
         self.entry.grid(row=0, column=0, padx=0, pady=30)
     
         self.password = ""
+
+
+        self.useLowerCases = True
+        self.useUpperCases = True
+        self.useDigits = True
+        self.usePunctuation = True
+
+    def checkBoxUpdate(self) -> None:
+        pass
 
     def basicPassGen(self, size : int = 15 ) -> None:
         if (type(size)==type("s")):
@@ -27,15 +76,22 @@ class App(customtkinter.CTk):
             except:
                 size = 15
 
-        alphabet = string.ascii_letters + string.digits + string.punctuation
+        alphabet = ""
+        if self.useLowerCases:
+            alphabet += string.ascii_lowercase
+        if self.useUpperCases:
+            alphabet += string.ascii_uppercase
+        if self.useDigits:
+            alphabet += string.digits
+        if self.usePunctuation:
+            alphabet += string.punctuation
+        
         self.password = ''.join(secrets.choice(alphabet) for i in range(size))
         
         self.label = customtkinter.CTkTextbox(self, state="normal", fg_color="white", text_color="red", width=200, height=60)
-        self.label.grid(row=2, column=0, padx=0, pady=60)
+        self.label.grid(row=6, column=0, padx=0, pady=60)
         self.label.insert("0.0", self.password)
-        self.label.configure(state="disabled")
-
-
+        # self.label.configure(state="disabled") # enable or disable the right of the user to modify the generated password
 
 app = App()
 app.mainloop()
